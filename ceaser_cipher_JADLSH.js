289abfa788ca64a9379f367628c6f0e831234b4f
TheSixDecadence <DelosSantosJ.10.3A@gmail.com>
2025-03-10 17:44:43 -0600

    Manage of the upper and lower case

--- ceaser_cipher_JADLSH.js
+++ ceaser_cipher_JADLSH.js
@@ -5,24 +5,28 @@
 
 function ceaser_encrypt(input) {
     abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
+    ABC = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
     var shift = 4; //you can change this value according to the shift you want
 
     for (var i = 0; i < input.length; i++) {
         
+        var isUpperCase = /[A-Z]/.test(input); //This will check if the input is uppercase or lowercase
+        var alphabet = isUpperCase ? ABC : abc;
+
         //This condition will check if the character is a space
         if(input[i] == " "){
             continue; //If it is a space, it will continue with the next iteration
         }
 
         //This will get the index of the character in the alphabet array
-        var index = abc.indexOf(input[i]);
+        var index = alphabet.indexOf(input[i]);
 
         //This will get the new index of the character after the shift
-        var newIndex = (index + shift) % abc.length;
+        var newIndex = (index + shift) % alphabet.length;
 
         //This will replace the character with the new character
-        input = input.substring(0, i) + abc[newIndex] + input.substring(i + 1);
+        input = input.substring(0, i) + alphabet[newIndex] + input.substring(i + 1);
     }
 
     console.log(input);
@@ -30,25 +34,29 @@ function ceaser_encrypt(input) {
 
 function ceaser_decrypt(input) {
     abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
+    ABC = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
     var shift = 4;
 
     for (var i = 0; i < input.length; i++) {
         
+        var isUpperCase = /[A-Z]/.test(input);
+        var alphabet = isUpperCase ? ABC : abc;
+
         if(input[i] == " "){
             continue;
         }
 
-        var index = abc.indexOf(input[i]);
-        var newIndex = (index - shift) % abc.length; //the difference between the encrypt and decrypt is the subtraction at the index/shift
+        var index = alphabet.indexOf(input[i]); //this will check if the character is uppercase or lowercase
+        var newIndex = (index - shift) % alphabet.length; //the difference between the encrypt and decrypt is the subtraction at the index/shift
 
-        input = input.substring(0, i) + abc[newIndex] + input.substring(i + 1); 
+        input = input.substring(0, i) + alphabet[newIndex] + input.substring(i + 1); 
     }
 
     console.log(input);
 }
 
-ceaser_encrypt("a b")
+ceaser_encrypt("A B")
 
-ceaser_decrypt("e f")
+ceaser_decrypt("E F")
 
